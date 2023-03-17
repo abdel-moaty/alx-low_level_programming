803f59191b444821b696ebd02a58029c62b191a6
abdel-moaty <ahmed.h.abdelmoaty@gmail.com>
2023-03-17 14:45:35 -0700

    Add C program to print single-digit numbers of base 10

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int D;
+
+	for (D = 0; D < 10; D++)
+		printf("%d", D);
+	printf("\n");
+	return (0);
+}
