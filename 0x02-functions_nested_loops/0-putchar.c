e7580bc23cf770ade6e6dce4feba09d1c55008a8
abdel-moaty <ahmed.h.abdelmoaty@gmail.com>
2023-03-21 10:57:05 -0700

    Add C program to print _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char c[10] = "_putchar\n";
+	int i;
+
+	for (i = 0; i < 9; i++)
+		_putchar(c[i]);
+	return (0);
+}
