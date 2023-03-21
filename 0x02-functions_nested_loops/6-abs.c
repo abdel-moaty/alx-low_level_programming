250094f2bae539a56208ac05192d79b1ea665b55
abdel-moaty <ahmed.h.abdelmoaty@gmail.com>
2023-03-21 14:38:34 -0700

    Add C program to compute absolute value of integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @i: integar
+ * Return: Absolute Value
+ */
+int _abs(int i)
+{
+	if (i >= 0)
+	{
+		return (i);
+	}
+	return (-i);
+}
