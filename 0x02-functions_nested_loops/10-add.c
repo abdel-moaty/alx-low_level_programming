43cc0866b4ad14c91eb1d171d9c7c498738a05b2
abdel-moaty <ahmed.h.abdelmoaty@gmail.com>
2023-03-21 16:10:41 -0700

    Add C program to return result of adding two integers

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * add - adds two integers and returns the result
+ * @i1: first integer
+ * @i2: second integer
+ * Return: Addition Result
+ */
+int add(int i1, int i2)
+{
+	return (i1 + i2);
+}
