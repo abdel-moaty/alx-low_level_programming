acf8e0738bef43c1ec2761e27f47c84a335295b5
abdel-moaty <ahmed.h.abdelmoaty@gmail.com>
2023-04-13 13:07:46 -0700

    Add C program to allocate memory using malloc

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - allocates memory using malloc
+ * @b: number of bytes
+ * Return: Pointer to Allocated Memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p = malloc(b);
+
+	if (p == NULL)
+	{
+		exit(98);
+	}
+	return (p);
+}
